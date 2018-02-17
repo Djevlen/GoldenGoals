d8d0949ef9b60c4142af16b35b4598c02b0b8a96
Thomas A Johansen <thomasaj@gmail.com>
2018-02-17 01:09:45 +0100

    core Data setup

---
+++ GoldenGoals/Core Data Model/Goal.swift
@@ -0,0 +1,14 @@
+//
+//  Goal.swift
+//  GoldenGoals
+//
+//  Created by Thomas Andre Johansen on 17/02/2018.
+//  Copyright © 2018 Appbryggeriet. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Goal: NSManagedObject {
+
+}
---
+++ GoldenGoals/Core Data Model/Tag.swift
@@ -0,0 +1,14 @@
+//
+//  Tag.swift
+//  GoldenGoals
+//
+//  Created by Thomas Andre Johansen on 17/02/2018.
+//  Copyright © 2018 Appbryggeriet. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Tag: NSManagedObject {
+
+}
