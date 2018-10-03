b1bd2bb0a0790a523fbd66b2f0bbbc3086d4c050
Thomas A Johansen <thomasaj@gmail.com>
2018-10-03 15:12:54 +0200

    Cleanup

--- GoldenGoals/About/CreditsCollectionViewController.swift
+++ GoldenGoals/About/CreditsCollectionViewController.swift
@@ -17,42 +17,22 @@ class CreditsCollectionViewController: UICollectionViewController {
     let mentos = Person(name: "Mentos", url: "https://www.appbryggeriet.com", image: UIImage(named: "creditsMentos")!, text: "Wooof! I'm Mentos. I can make my eyes sparkle in a way that forces you to feed me. More food, please! Food is life. 😇")
     let dent = Person(name: "Dent", url: "https://www.appbryggeriet.com", image: UIImage(named: "creditsDent")!, text: "Bark! I'm Dent. Bark! I'm fast! Bark! I'm crazy! Bark! I also destroy all of Mentos' favorite toys on a daily basis! 😘")
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    fileprivate func populateCredits() {
         people.append(thomas)
         people.append(trine)
         people.append(mentos)
         people.append(dent)
-        
-
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populateCredits()
     }
-    */
-
     // MARK: UICollectionViewDataSource
-
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
@@ -70,36 +50,6 @@ class CreditsCollectionViewController: UICollectionViewController {
         return cell
     }
     
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let theURL = people[indexPath.row].url else {return}
         let thanksURL = URL(string:theURL)
