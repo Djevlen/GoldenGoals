1571c4de4e4fdd41f6d8fe1ee224deda927d27cd
Thomas A Johansen <thomasaj@gmail.com>
2018-10-03 14:11:42 +0200

    Cleanup

--- GoldenGoals/About/SpecialThanksTableViewController.swift
+++ GoldenGoals/About/SpecialThanksTableViewController.swift
@@ -8,16 +8,15 @@
 
 import UIKit
 
-struct Person{
-    var name: String
-    var url: String
-    var image: UIImage
-}
-
 class SpecialThanksTableViewController: UITableViewController {
     var people: [Person] = []
     let bjorn = Person(name: "Bjørn Rostad", url: "http://www.bjornrostad.no", image: UIImage(named: "thanksBjorn")!)
     let mats = Person(name: "Mats Lindh", url: "http://www.matslindh.no", image: UIImage(named: "thanksMats")!)
+//    let marius = Person(name: "Marius Lindh", url: <#T##String#>, image: <#T##UIImage#>:
+//    let eirik = Person(name: "Eirik Jensen", url: <#T##String#>, image: <#T##UIImage#>
+//    let sean = Person(name: "Sean Allen", url: <#T##String#>, image: <#T##UIImage#>
+//    let john = Person(name: "John Sundell", url: "https://www.swiftbysundell.com", image: PUT IMAGE HERE)
+//    let paul = Person(name: "Paul Hudson", url: "https://twitter.com/twostraws", image: <#T##UIImage#>
     let icons8 = Person(name: "Icons8.com", url: "http://www.icons8.com", image: UIImage(named: "thanksIcon8")!)
     
 
@@ -29,26 +28,18 @@ class SpecialThanksTableViewController: UITableViewController {
         
         people.append(icons8)//should always be the last one in the list due to footer text
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
-
+    //only returning one section, the complete list of people I'll thank
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        print("people count: \(people.count)")
         return people.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let person = people[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "thanksCell", for: indexPath)
@@ -66,55 +57,6 @@ class SpecialThanksTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let thanksURL = URL(string: people[indexPath.row].url)!
-        print("didselectrow \(thanksURL)")
         UIApplication.shared.open(thanksURL)
     }
-
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ GoldenGoals/Support/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  GoldenGoals
+//
+//  Struct used for displaying people thanked in the ABOUT section of the app.
+//
+//  Created by Thomas Andre Johansen on 03/10/2018.
+//  Copyright © 2018 Appbryggeriet. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Person{
+    var name: String
+    var url: String
+    var image: UIImage
+}
